cfb50e50e594602a2dbf45e2b5c9aaf613841c9a
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-01-21 13:17:46 -0600

    REST API Video

---
+++ server/modelos/video.js
@@ -0,0 +1,9 @@
+class Video{
+    constructor(id,nombre){
+        this.id=id;
+        this.nombre=nombre;
+    }
+}
+
+
+module.exports=Video;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const datos=require('../store/data.json');
+const Video=require('./modelos/video');
 
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(express.json());
+
+
+
+
+app.get('/video/:id',(req,res)=>{
+    res.json(datos);
+});
 
 app.get('/video',(req,res)=>{
     res.json({
@@ -10,12 +24,19 @@ app.get('/video',(req,res)=>{
 });
 
 app.post('/video',(req,res)=>{
+const body=req.body;
     res.json({
-        mensaje:'post video'
+        mensaje:body
     });
 });
 
 app.put('/video/:id',(req,res)=>{
+    res.json({
+        mensaje:'put video'
+    });
+});
+
+app.put('/upload/:id',(req,res)=>{
     res.json({
         mensaje:'put video'
     });
